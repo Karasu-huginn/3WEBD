34324d9eea5136ce5655f80f47b2d061b70b24ba
LucasMillart <lucas.millart@gmail.com>
2026-01-31 22:43:57 +0100

    feat: ajout tailwindcss pour styling et header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Link } from "react-router";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      {/*LOGO*/}
+      <div>
+        Town <span>Library</span>
+      </div>
+
+      {/*SEARCH*/}
+      <div></div>
+
+      {/**Home */}
+      <Link to="/">Home</Link>
+      {/*ADVANCED SEARCH LINK */}
+      <Link to="/advanced-search" className="hover:text-blue-200">
+        Advanced Search
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
