ea34711b8d1597ca85ce6365f8fedeab2a53a8ab
LucasMillart <lucas.millart@gmail.com>
2026-02-03 13:42:13 +0100

    fix: add key to maps

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -3,7 +3,7 @@ import { useBookDetails } from "../hooks/useBookDetails";
 
 const BookDetails = () => {
   const [searchParams] = useSearchParams();
-  const key = searchParams.get("key") || ""
+  const key = searchParams.get("key") || "";
   const { data, isLoading, error } = useBookDetails(key);
 
   if (isLoading) {
@@ -22,50 +22,45 @@ const BookDetails = () => {
     );
   }
 
-
-  return <div>
-    <p>{data?.title}</p>
-    <p>{data?.description.value}</p>
-    {
-      // Genres du livre (drama, romance, policier etc...)
-      data?.subjects.map((subject) => (
-        <p>{subject}</p>
+  return (
+    <div>
+      <p>{data?.title}</p>
+      <p>{data?.description.value}</p>
+      {// Genres du livre (drama, romance, policier etc...)
+      data?.subjects?.map((subject) => (
+        <p key={subject}>{subject}</p>
       ))}
-    {
-      // Personnages du livre
-      data?.subject_people.map((subject_person) => (
-        <p>{subject_person}</p>
+      {// Personnages du livre
+      data?.subject_people?.map((subject_person) => (
+        <p key={subject_person}>{subject_person}</p>
       ))}
-    {
-      // Ville et Pays
-      data?.subject_places.map((subject_place) => (
-        <p>{subject_place}</p>
+      {// Ville et Pays
+      data?.subject_places?.map((subject_place) => (
+        <p key={subject_place}>{subject_place}</p>
       ))}
-    {
-      // date (année uniquement)
-      data?.subject_times.map((subject_time) => (
-        <p>{subject_time}</p>
+      {// date (année uniquement)
+      data?.subject_times?.map((subject_time) => (
+        <p key={subject_time}>{subject_time}</p>
       ))}
 
-    {
-      // extraits d'auteur (requête(s) supplémentaire(s) pour obtenir les noms d'auteurs par leur clé ?)
-      data?.excerpts.map((excerpt) => (
-        <div>
+      {// extraits d'auteur (requête(s) supplémentaire(s) pour obtenir les noms d'auteurs par leur clé ?)
+      data?.excerpts?.map((excerpt) => (
+        <div key={excerpt.excerpt}>
           <p>{excerpt.excerpt}</p>
           <p>{excerpt.author.key}</p>
           <p>{excerpt.comment}</p>
         </div>
       ))}
-    <p>{data?.created.value}</p>
-    <p>{data?.last_modified.value}</p>
-    {
-      // external links to the book
-      data?.links.map((link) => (
-        <a href={link.url}>{link.title}</a>
-      ))
-    }
-
-  </div>;
+      <p>{data?.created.value}</p>
+      <p>{data?.last_modified.value}</p>
+      {// external links to the book
+      data?.links?.map((link) => (
+        <a key={link.url} href={link.url}>
+          {link.title}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default BookDetails;
