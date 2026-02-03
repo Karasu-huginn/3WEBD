fa4968400868e67479ffcfb35c69245fe96176a7
LucasMillart <lucas.millart@gmail.com>
2026-02-03 20:42:21 +0100

    style: component BookDetails and BookWiki

--- src/components/BookWiki.tsx
+++ src/components/BookWiki.tsx
@@ -1,29 +1,34 @@
 import { useBookWiki } from "../hooks/useBookWiki";
 
 interface BookWikiProps {
-    author_name: string;
-    book_title: string;
+  author_name: string;
+  book_title: string;
 }
 
 export const BookWiki = (props: BookWikiProps) => {
-    const { data, isLoading, error } = useBookWiki(`${props.book_title}`);
-    //const { data, isLoading, error } = useBookWiki(`${props.author_name} ${props.book_title}`);
+  const { data, isLoading, error } = useBookWiki(`${props.book_title}`);
+  //const { data, isLoading, error } = useBookWiki(`${props.author_name} ${props.book_title}`);
 
-    if (isLoading) {
-        return (
-            <div className="p-8">
-                <p className="text-center">Loading...</p>
-            </div>
-        );
-    }
+  if (isLoading) {
+    return (
+      <div className="p-8">
+        <p className="text-center">Loading...</p>
+      </div>
+    );
+  }
 
-    if (error) {
-        return (
-            <div className="p-8">
-                <p className="text-center text-red-500">Error: {error.message}</p>
-            </div>
-        );
-    }
+  if (error) {
+    return (
+      <div className="p-8">
+        <p className="text-center text-red-500">Error: {error.message}</p>
+      </div>
+    );
+  }
 
-    return <iframe src={`https://en.wikipedia.org/wiki/${data?.key}`}></iframe>
-}
+  return (
+    <iframe
+      className="border border-gray-300 rounded-2xl p-5 w-2/5"
+      src={`https://en.wikipedia.org/wiki/${data?.key}`}
+    ></iframe>
+  );
+};
